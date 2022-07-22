de538eee37395df2688f8a423c10fbfb0ea251ad
jimtin <jameshin84@gmail.com>
2022-07-23 08:08:40 +1000

    Working Initial Main function

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Initial Main File Setup
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_welcome():
+    # Welcome to your Automated Backtester for Meta Trader 5! You're well on your way!
+    print(f'Welcome to Automated Trading Backtester for Meta Trader 5!')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print_welcome()
